f3d7ba6e5b5b61995a10323c9eae153ede58e694
Ivan Piskunov <44296554+D3One@users.noreply.github.com>
2025-08-29 13:36:10 +0300

    Create build.sh

---
+++ Custom Docker Image/build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -euo pipefail
+
+temp_build_dir='temp_build_dir'
+username='ivanpiskunov'
+image_name='sectkit'
+build_params="$@"
+full_image_name=${username}/${image_name}
+full_image_name_latest=${full_image_name}:latest
+
+if [[ -n "${GITHUB_WORKFLOW:-}" ]];then
+    # we are running in CI
+    # get image tag from ref/tag
+    full_image_name=$full_image_name:${SOURCE_TAG}
+fi
+
+
+function clean_up(){
+    rm -rf ${temp_build_dir}
+}
+
+trap clean_up ERR SIGINT
+
+mkdir -p ${temp_build_dir}
+
+cp docker-image/Dockerfile ${temp_build_dir}
+
+cp package*.json ${temp_build_dir}
+
+cp -R dist ${temp_build_dir}
+
+docker build ${build_params} -t ${full_image_name} -t ${full_image_name_latest} ${temp_build_dir}
+
+
+if [[ -n "${GITHUB_WORKFLOW:-}" ]];then
+    
+    # push only if building from CI
+    
+    docker push ${username}/${image_name}
+fi
+
+clean_up
